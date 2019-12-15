86cc01c96294bec21fd80627062ab2888acb608c
Your NameRobin <rlifshitz19@gmail.com>
2019-12-15 15:25:36 -0800

    Basic function

---
+++ game.js
@@ -0,0 +1,16 @@
+function play(myChoice) {
+
+    const rps = ['rock', 'paper', 'scissors'];
+
+    let randomNum = Math.floor((Math.random() * 3) + 1);
+
+    let computerChoice = rps[randomNum];
+
+
+
+
+
+
+
+
+}
