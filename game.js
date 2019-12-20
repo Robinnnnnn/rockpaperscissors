4dcc3b0819cfe3709ba6fd3522082d2cc0a9e5bc
Your NameRobin <rlifshitz19@gmail.com>
2019-12-20 12:11:49 -0800

    rps w/ frontend

--- game.js
+++ game.js
@@ -1,136 +1,164 @@
 let playerScore = 0;
 let compScore = 0;
 const rps = ["rock", "paper", "scissors"];
-var myChoiceUp;
-const rock = "rock";
-const paper = "paper";
-const scissors = "scissors";
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+let text2 = document.querySelector('#text2');
+let text1 = document.querySelector('#text1');
+let startButton = document.querySelector('#start');
 let myChoice;
 
-function getMyChoice() {
-    return (myChoice = prompt("Choose your weapon"));
-}
+rock.addEventListener('click', () => {
+    myChoice = "Rock";
+    rock.style.cssText = 'background-color: rgb(34, 146, 211)';
+    scissors.style.cssText = 'background-color: rgb(148, 130, 153)';
+    paper.style.cssText = 'background-color: rgb(148, 130, 153)';
+});
+paper.addEventListener('click', () => {
+    myChoice = "Paper";
+    paper.style.cssText = 'background-color: rgb(34, 146, 211)';
+    scissors.style.cssText = 'background-color: rgb(148, 130, 153)';
+    rock.style.cssText = 'background-color: rgb(148, 130, 153)';
+});
+scissors.addEventListener('click', () => {
+    myChoice = "Scissors";
+    scissors.style.cssText = 'background-color: rgb(34, 146, 211)';
+    rock.style.cssText = 'background-color: rgb(148, 130, 153)';
+    paper.style.cssText = 'background-color: rgb(148, 130, 153)';
+});
+
+startButton.addEventListener('click', () => {
+    start();
+    rock.style.cssText = 'background-color: rgb(148, 130, 153)';
+    paper.style.cssText = 'background-color: rgb(148, 130, 153)';
+    scissors.style.cssText = 'background-color: rgb(148, 130, 153)';
+});
+
+// v0.0.01
+// function getMyChoice() {
+//     return (myChoice = prompt("Choose your weapon"));
+// }
+
+
 
 function weStillPlayin() {
     if (playerScore < 5 && compScore < 5) {
-        console.log("Keep Playing!");
+        text2.textContent = "Keep Playing!";
     } else if ((playerScore = 5)) {
-        console.log("You win, nice!");
+        text2.textContent = "You win, nice!";
         playerScore = 0;
         omputerScore = 0;
     } else if ((compScore = 5)) {
-        console.log("Computer Wins!");
+        text2.textContent = "Computer Wins!";
         playerScore = 0;
         computerScore = 0;
     }
-}
+};
 
 function opponentChoice() {
     let randomNum = Math.floor(Math.random() * 3);
     return rps[randomNum].toUpperCase();
-}
+};
+
+function checkForWeapon() {
+    if (myChoice === undefined) {
+        text2.textContent = "Please Select a Weapon Before Beginning Battle!";
+    } else {
+        opponentChoice();
+    };
+};
 
 function play() {
     let computerChoice = opponentChoice();
-    myChoice = myChoice.toUpperCase();
-
-    //Run when I choose rock
 
     function rock() {
         if (computerChoice === "SCISSORS") {
             playerScore++;
-            console.log(
+            text1.textContent =
                 "Nice, your score is " +
                 playerScore +
                 ". Opponent had " +
                 computerChoice +
-                "."
-            );
+                ".";
         } else if (computerChoice === "PAPER") {
             compScore++;
-            console.log(
+            text1.textContent =
                 "Woops, your score is " +
                 playerScore +
                 ". Your opponent has " +
                 computerChoice +
                 " & " +
                 compScore +
-                " points."
-            );
+                " points.";
         } else if (computerChoice === "ROCK") {
-            console.log("Draw! Try Again!");
+            text1.textContent = "Draw! Try Again!";
         }
-    }
+    };
 
-    //Run when I choose scissors
     function scissors() {
         if (computerChoice === "PAPER") {
             playerScore++;
-            console.log(
+            text1.textContent =
                 "Nice, your score is " +
                 playerScore +
                 ". Opponent had " +
                 computerChoice +
-                "."
-            );
+                ".";
         } else if (computerChoice === "ROCK") {
             compScore++;
-            console.log(
+            text1.textContent =
                 "Woops, your score is " +
                 playerScore +
                 ". Your opponent has " +
                 computerChoice +
                 " & " +
                 compScore +
-                " point."
-            );
+                " point.";
         } else if (computerChoice === "SCISSORS") {
-            console.log("Draw! Try Again!");
+            text1.textContent = "Draw! Try Again!";
         }
     }
 
-    //Run when I choose paper
     function paper() {
         if (computerChoice === "ROCK") {
             playerScore++;
-            console.log(
+            text1.textContent =
                 "Nice, your score is " +
                 playerScore +
                 ". Opponent had " +
                 computerChoice +
-                "."
-            );
+                ".";
         } else if (computerChoice === "SCISSORS") {
             compScore++;
-            console.log(
+            text1.textContent =
                 "Woops, your score is " +
                 playerScore +
                 ". Your opponent has " +
                 computerChoice +
                 " & " +
                 compScore +
-                " point."
-            );
+                " point.";
         } else if (computerChoice === "PAPER") {
-            console.log("Draw! Try Again!");
+            text1.textContent = "Draw! Try Again!";
         }
     }
 
-    if (myChoice === "ROCK") {
+    if (myChoice === "Rock") {
         rock();
-    } else if (myChoice === "PAPER") {
+    } else if (myChoice === "Paper") {
         paper();
-    } else if (myChoice === "SCISSORS") {
+    } else if (myChoice === "Scissors") {
         scissors();
-    }
+    };
 
     weStillPlayin();
-}
+};
+
+
 
 function start() {
-    getMyChoice();
-    opponentChoice();
+    checkForWeapon();
     play();
-}
 
-let rovi = () => {};
+};
